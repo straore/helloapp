20cd6566f490c72c0ca8b4265d465b1f2b1d1b3c
Siaka <siaka.techbox@gmail.com>
2019-03-12 00:17:36 +0100

    initialisation of simple app to be kubernetized

---
+++ src/main/java/com/straore/helloapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.straore.helloapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "hello mike";
+    }
+
+}
